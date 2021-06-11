6708593abd48a8aff242f270b4a098f78fa81e37
shidosaiga <shidosaigaapirak@gmail.com>
2021-06-11 12:28:17 +0700

    update2

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
-    echo ("GG")
-    
+    $a = 60;
+
+    if ($a <=50) {
+        echo "D<br>";    
+    }elseif ($a <=60) {
+        echo "C<br>";
+    }elseif ($a <=70) {
+        echo "B<br>";
+    }elseif ($a <=80) {
+        echo "A<br>";
+    }else {
+        echo "F<br>";
+    }
+    echo "End Program<br>";
 ?>
